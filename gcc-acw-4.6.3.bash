f5d11f7a0b0afd07365096db1cf51c982bf2e5cb
Dylan Maxwell <planar.geometry@gmail.com>
2012-08-23 17:21:55 -0600

    Update installation to use the new 'g++-acw' script

--- gcc-acw-4.6.3.bash
+++ gcc-acw-4.6.3.bash
@@ -14,12 +14,12 @@ rcheck cd "$DIRNAME/$BUILDNAME"
 
 
 if [ ! -d "gcc-acw" ]; then
-	rcheck git clone  "git@github.com:xyplane/gcc-acw.git"
+	rcheck git clone -b "gcc-4.6.3" "git@github.com:xyplane/gcc-acw.git"
 fi
 
 rcheck cd "gcc-acw"
 
-rcheck sudo cp -f "gcc-acw" "/usr/local/bin/cc-acw-4.6.3"
 rcheck sudo cp -f "gcc-acw" "/usr/local/bin/gcc-acw-4.6.3"
-rcheck sudo cp -f "gcc-acw" "/usr/local/bin/c++-acw-4.6.3"
-rcheck sudo cp -f "gcc-acw" "/usr/local/bin/g++-acw-4.6.3"
+rcheck sudo cp -f "g++-acw" "/usr/local/bin/g++-acw-4.6.3"
+rcheck sudo ln -sf "gcc-acw-4.6.3" "/usr/local/bin/cc-acw-4.6.3"
+rcheck sudo ln -fs "g++-acw-4.6.3" "/usr/local/bin/c++-acw-4.6.3"
