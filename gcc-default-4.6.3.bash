a6af4a1adad1ee05236cf74f1d12f46e24edbda5
Dylan Maxwell <planar.geometry@gmail.com>
2012-08-23 17:20:55 -0600

    Fix path problems with default symlinks (was 'usr/local' should be '/usr/local')

--- gcc-default-4.6.3.bash
+++ gcc-default-4.6.3.bash
@@ -12,11 +12,10 @@ BASENAME=`basename "$0"`
 #rcheck mkdir -p "$DIRNAME/$BUILDNAME"
 #rcheck cd "$DIRNAME/$BUILDNAME"
 
-rcheck mkdir -p usr/local/bin
-rcheck	ln -sf  "cc-acw-4.6.3"    "usr/local/bin/cc"
-rcheck	ln -sf  "c++-acw-4.6.3"   "usr/local/bin/c++"
-rcheck	ln -sf  "gcc-acw-4.6.3"   "usr/local/bin/gcc"
-rcheck	ln -sf  "g++-acw-4.6.3"   "usr/local/bin/g++"
-rcheck	ln -sf  "cpp-4.6.3"       "usr/local/bin/cpp"
-rcheck	ln -sf  "gcov-4.6.3"      "usr/local/bin/gcov"
-rcheck	ln -sf  "gfortran-4.6.3"  "usr/local/bin/gfortran"
+rcheck sudo ln -sf  "cc-acw-4.6.3"    "/usr/local/bin/cc"
+rcheck sudo ln -sf  "c++-acw-4.6.3"   "/usr/local/bin/c++"
+rcheck sudo ln -sf  "gcc-acw-4.6.3"   "/usr/local/bin/gcc"
+rcheck sudo ln -sf  "g++-acw-4.6.3"   "/usr/local/bin/g++"
+rcheck sudo ln -sf  "cpp-4.6.3"       "/usr/local/bin/cpp"
+rcheck sudo ln -sf  "gcov-4.6.3"      "/usr/local/bin/gcov"
+rcheck sudo ln -sf  "gfortran-4.6.3"  "/usr/local/bin/gfortran"
